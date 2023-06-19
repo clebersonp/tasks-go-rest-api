426b4d823fe4b0c7b806c2167da252a6253f2f9c
Cleberson <pauluci.cleberson@gmail.com>
2023-06-19 13:53:43 -0300

    Change insert function

--- models/insert.go
+++ models/insert.go
@@ -4,8 +4,8 @@ import (
 	"github.com/clebersonp/tasks-go-rest-api/db"
 )
 
-// Insert inserts into tasks table the task
-func Insert(task Task) (id int64, err error) {
+// Insert inserts into tasks table the task and returns the task
+func Insert(task Task) (t Task, err error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
 		return
@@ -13,7 +13,9 @@ func Insert(task Task) (id int64, err error) {
 	defer conn.Close()
 
 	sql := `INSERT INTO tasks (title, description, done) VALUES ($1, $2, $3) RETURNING id`
-	err = conn.QueryRow(sql, task.Title, task.Description, task.Done).Scan(&id)
-
-	return
+	err = conn.QueryRow(sql, task.Title, task.Description, task.Done).Scan(&task.ID)
+	if err != nil {
+		return
+	}
+	return task, nil
 }
