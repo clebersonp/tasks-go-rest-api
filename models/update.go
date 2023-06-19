200e4502c27018cdadad6a27a865179983989f7a
Cleberson <pauluci.cleberson@gmail.com>
2023-06-19 09:53:03 -0300

    Implemented the update operation

--- models/update.go
+++ models/update.go
@@ -1,8 +1,19 @@
 package models
 
-// Update update a given task in the database
+import "github.com/clebersonp/tasks-go-rest-api/db"
+
+// Update updates the given task in the database and returns the number of rows affected or the error
 func Update(id int64, task Task) (int64, error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
 
-	// TODO implement
-
+	sql := `UPDATE tasks SET title=$2, description=$3, done=$4 WHERE id=$1`
+	result, err := conn.Exec(sql, id, task.Title, task.Description, task.Done)
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
 }
