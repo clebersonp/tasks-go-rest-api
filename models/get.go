f5ba8482f101eaa410bdba71db3f85c6b3c96d12
Cleberson <pauluci.cleberson@gmail.com>
2023-06-19 00:40:56 -0300

    Created get by id operation

--- models/get.go
+++ models/get.go
@@ -1,3 +1,17 @@
 package models
 
-// TODO continue...
+import "github.com/clebersonp/tasks-go-rest-api/db"
+
+// Get returns one task by it id
+func Get(id int64) (task Task, err error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	row := conn.QueryRow(`SELECT * FROM tasks WHERE id=$1`, id)
+	err = row.Scan(&task.ID, &task.Title, &task.Description, &task.Done)
+
+	return
+}
