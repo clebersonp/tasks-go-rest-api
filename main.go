48104002a30374ec0c426fcf5d4d4978e7a9a3c2
Cleberson <pauluci.cleberson@gmail.com>
2023-06-19 22:00:23 -0300

    Added router config using third-party chi lib

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/clebersonp/tasks-go-rest-api/configs"
+	"github.com/clebersonp/tasks-go-rest-api/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
@@ -11,14 +16,30 @@ func main() {
 	// config the log formater
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
 
-	if err := configs.Load(); err != nil {
-		log.Fatal(err)
-	}
+	// create a router. https://github.com/go-chi/chi
+	r := chi.NewRouter()
 
-	dbConfig := configs.DB()
-	log.Printf("DB Config: Host: %v, Port: %v, User: %v, Password: %v, Database: %v\n",
-		dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.Password, dbConfig.Database)
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
+	// Rest routes for "tasks" resource
+	r.Route("/tasks", func(r chi.Router) {
+		r.Get("/", handlers.List)    // GET /tasks
+		r.Post("/", handlers.Create) // POST /tasks
+
+		// subrouters:
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", handlers.Get)       // GET /tasks/123
+			r.Put("/", handlers.Update)    // PUT /tasks/123
+			r.Delete("/", handlers.Delete) // DELETE /tasks/123
+		})
+	})
+
+	// starts the rest api server
 	serverPort := configs.ServerPort()
-	log.Printf("Server Port: %v\n", serverPort)
+	log.Println("Started the rest api server at port:", serverPort)
+	http.ListenAndServe(fmt.Sprintf("localhost:%s", serverPort), r)
 }
